e1726eb46aec83edd12e1e07a0a78b68edcb3279
remon <Remon123youssf@gmail.com>
2025-07-07 03:32:20 +0300

    create Basis

---
+++ app/src/main/java/com/example/newsapp/Model/newsResponse/News.kt
@@ -0,0 +1,48 @@
+package com.example.newsapp.Model.newsResponse
+
+import com.example.newsapp.Model.SourcesResponse.Source
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+import java.text.SimpleDateFormat
+import java.util.Date
+
+@Parcelize
+data class News(
+
+    @field:SerializedName("publishedAt")
+    val publishedAt: String? = null,
+
+    @field:SerializedName("author")
+    val author: String? = null,
+
+    @field:SerializedName("urlToImage")
+    val urlToImage: String? = null,
+
+    @field:SerializedName("description")
+    val description: String? = null,
+
+    @field:SerializedName("source")
+    val source: Source? = null,
+
+    @field:SerializedName("title")
+    val title: String? = null,
+
+    @field:SerializedName("url")
+    val url: String? = null,
+
+    @field:SerializedName("content")
+    val content: String? = null
+) : Parcelable{
+    fun getPublishedAtInMillis():Long?{
+        try {
+            val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+            //2025-01-31T11:24:40Z
+            val dateTime : Date? = publishedAt?.let { simpleDateFormat.parse(it) }
+            return dateTime?.time
+
+        }catch (ex:Exception){
+            return null
+        }
+    }
+}
